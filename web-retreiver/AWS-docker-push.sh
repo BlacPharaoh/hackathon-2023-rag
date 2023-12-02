34683c92349e71aa7bec9410dad6dbccea006816
Bassem Yacoube <125713079+AI-Bassem@users.noreply.github.com>
2023-12-01 17:40:06 -0800

    Delete

--- web-retreiver/AWS-docker-push.sh
+++
@@ -1,3 +0,0 @@
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 186900524924.dkr.ecr.us-east-1.amazonaws.com
-docker tag f7200391d23a 186900524924.dkr.ecr.us-east-.amazonaws.com/llama2-lambda:v1
-docker push 186900524924.dkr.ecr.us-east-1.amazonaws.com/llama2-lambda:v1
